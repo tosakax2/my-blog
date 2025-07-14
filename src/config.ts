d11c2bb74b94097ff206a23a0fc7071a1044ea25
tosakax2 <tosakax2@gmail.com>
2025-07-14 10:11:17 +0900

    Add SEO

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,22 @@
 export const SITE_TITLE = "T2-Lab";
 export const SITE_DESCRIPTION = "tosakax2の個人ブログ";
+export const SITE_URL = "https://t2-lab.pages.dev/";
 export const LANGUAGE = "ja";
 export const AUTHOR = "tosakax2";
+
+export const SEO_DEFAULTS = {
+  title: SITE_TITLE,
+  description: SITE_DESCRIPTION,
+  openGraph: {
+    basic: {
+      title: SITE_TITLE,
+      type: "website",
+      image: `${SITE_URL}/images/default-image`,
+      url: SITE_URL,
+    },
+  },
+  twitter: {
+    site: "@tosakax2",
+    card: "summary_large_image",
+  },
+};
