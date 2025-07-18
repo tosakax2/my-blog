3a2105fe55d99611a5f7ed76f7fa00ed16e3dbba
tosakax2 <tosakax2@gmail.com>
2025-07-18 10:40:30 +0900

    Add article

--- public/js/scramble-title.js
+++ public/js/scramble-title.js
@@ -6,28 +6,18 @@
  * @param {number} options.interval - スクランブル更新間隔ms
  * @param {number} options.totalDuration - 全体収束時間ms
  */
-export function scrambleTitle({
-  selector = "h1[data-scramble]",
-  letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-",
-  interval = 40,
-  totalDuration = 800,
-} = {}) {
+export function scrambleTitle({ selector = "h1[data-scramble]", letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{};:'\",.<>?/|\\~", interval = 40, totalDuration = 800 } = {}) {
   const els = document.querySelectorAll(selector);
 
   els.forEach((el) => {
     const htmlEl = el;
-    const finalText =
-      htmlEl.dataset.scrambleText ||
-      htmlEl.textContent ||
-      "";
+    const finalText = htmlEl.dataset.scrambleText || htmlEl.textContent || "";
     const length = finalText.length;
     let display = Array.from(finalText);
     let fixed = Array(length).fill(false);
     let scrambleTimer;
 
-    const stepsToFix = [...Array(length).keys()].filter(
-      (i) => finalText[i] !== " "
-    );
+    const stepsToFix = [...Array(length).keys()].filter((i) => finalText[i] !== " ");
     const charsToFix = stepsToFix.length;
     const perChar = totalDuration / charsToFix;
 
